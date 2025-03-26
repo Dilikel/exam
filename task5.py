d357b650ca6e4c4a553a8cdb832c8df9192ddf1f
vovan101207 <shiryayev_vova@bk.ru>
2025-03-27 00:31:33 +0300

    1.2

--- task5.py
+++ task5.py
@@ -9,8 +9,32 @@ def f(n):
         else:
             x += '1'
         decimal_number = int(x, 2)
-        nums = [int(digit) for digit in str(decimal_number)]
+        nums = [int(num) for num in str(decimal_number)]
         sum_nums = sum(nums)
     return int(x, 2)
 
-print(f(17))
+
+lower_bound = 987654321
+upper_bound = 2123456789
+count = 0
+max_n = 10000000
+progress_step = max_n // 100
+
+print("Processing: ", end="", flush=True)
+n = 1
+while True:
+    R = f(n)
+    if R > upper_bound:
+        break
+    if lower_bound <= R <= upper_bound:
+        count += 1
+
+    if n % progress_step == 0:
+        progress_percent = (n // progress_step)
+        print(f"\rProcessing: {progress_percent}%", end="", flush=True)
+
+    n += 1
+
+print("\nProcessing: 100%")
+print("\nDone!")
+print(f"Count: {count}")
